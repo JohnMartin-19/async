086c42c5121b58c61b772ea7f077f3ecc8190e5a
John <johnnkonge2020@gmail.com>
2024-03-12 20:36:37 +0300

    callback hell solved

--- async.js
+++ async.js
@@ -13,3 +13,45 @@ function getStudents(){
     },2000)
 }
 getStudents()
+
+
+/** 
+ * Callback
+ * Hell
+ * 
+*/
+
+console.log("First-line");
+getStudent(1, displayStudent);
+console.log("Last-line");
+// Defining Callback functions
+function displayStudent(student) {
+    console.log("Student", student);
+    getSubjects(student.id, displaySubjects);
+}
+function displaySubjects(subjects) {
+    console.log(subjects);
+    getMarks(subjects[0], displayMarks);
+}
+function displayMarks(mark) {
+    console.log("Mark", mark);
+}
+// Our Main functions
+function getStudent(id, callback) {
+    setTimeout(() => {
+    console.log("Getting data from Database");
+    callback({ name: "Bob", id: id });
+}, 2000);
+}
+function getSubjects(id, callback) {
+    setTimeout(() => {
+    console.log("Getting subject of student", id);
+    callback(["Maths", "Science", "Computer"]);
+}, 2000);
+}
+function getMarks(subject, callback) {
+    setTimeout(() => {
+    console.log("Getting marks of", subject);
+    callback(80);
+}, 2000);
+}
